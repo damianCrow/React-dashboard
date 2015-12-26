32873109c8707781c8290a83e7db420d7e310000
Giulio <giulio.r@interstateteam.com>
2015-12-26 18:24:02 +0000

    title

--- index.php
+++ index.php
@@ -5,7 +5,7 @@
   <head>
     <meta charset="utf-8" />
     <meta name="viewport" content="width=device-width, initial-scale=1.0" />
-    <title>BMD</title>
+    <title>BMD - v2.0</title>
     <link rel="stylesheet" href="css/foundation.css" />
 	<link rel="stylesheet" href="css/style.css" />
     <script src="js/vendor/modernizr.js"></script>
