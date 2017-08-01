ca20938d3c4ed6cb9c5561a1fdc371ff001ee853
Simon Batty <simon@psymon.co.uk>
2017-08-01 18:25:46 +0100

    Fixed album art shadow and checked for Sonos streams.

--- src/components/organisms/SonosTrack/index.js
+++ src/components/organisms/SonosTrack/index.js
@@ -2,6 +2,7 @@ import PropTypes from 'prop-types'
 import React from 'react'
 import styled, { css } from 'styled-components'
 import { fonts } from 'components/globals'
+import startsWith from 'lodash/startsWith'
 
 import { WaveAnimation } from 'components'
 
@@ -91,7 +92,8 @@ const AlbumArtCurrent = styled(AlbumArtNext)`
   width: 50%;
   left: 25%;
   z-index: 2;
-  box-shadow: 5px 5px 25px 3px #333;
+  filter: drop-shadow(0px 0px 2px rgba(0,0,0,0.5));
+  // box-shadow: 5px 5px 25px 3px #333;
 `
 
 const AlbumArtPrevious = styled(AlbumArtNext)`
@@ -131,7 +133,7 @@ const SonosTrack = ({ ...props, trackInfo, previousTrack }) => {
         </AlbumArtContainer>
         <TrackInfo>
           <Track>
-            {currentTrack.title}
+            {startsWith(trackInfo.currentTrack.title, 'x-sonosapi-stream') ? '' : trackInfo.currentTrack.title}
           </Track>
           <Artist>
             {currentTrack.artist}
