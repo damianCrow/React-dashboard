e29f3fc6dff0b4725aad68780f478d1ab7605ae2
Damian Whyte <damianwhyte@Inter209-2.local>
2017-08-04 10:27:12 +0100

    wave animation modified.

--- src/components/organisms/WaveAnimation/index.js
+++ src/components/organisms/WaveAnimation/index.js
@@ -7,44 +7,44 @@ class WaveAnimation extends Component {
     super(props)
 
     this.waveConfig = {
-      speed: 5,
+      speed: 3,
       ease: 'SineInOut',
       wavesWidth: '100%',
       waves: [
         {
           timeModifier: 4,
           lineWidth: 1,
-          amplitude: -25,
+          amplitude: -15,
           wavelength: 25,
         },
         {
           timeModifier: 2,
           lineWidth: 1,
-          amplitude: -10,
+          amplitude: -2,
           wavelength: 30,
         },
         {
           timeModifier: 1,
           lineWidth: 1,
-          amplitude: -30,
+          amplitude: -20,
           wavelength: 30,
         },
         {
           timeModifier: 3,
           lineWidth: 1,
-          amplitude: 40,
+          amplitude: 20,
           wavelength: 40,
         },
         {
           timeModifier: 0.5,
           lineWidth: 1,
-          amplitude: -60,
+          amplitude: -40,
           wavelength: 60,
         },
         {
           timeModifier: 1.3,
           lineWidth: 1,
-          amplitude: -40,
+          amplitude: -30,
           wavelength: 40,
         },
       ],
