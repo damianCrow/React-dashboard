c75b6294c1abe1b484703df940e66c37075653af
Simon Batty <simon@psymon.co.uk>
2017-11-20 12:03:35 +0000

    Removed the cursor again for staging.

--- src/components/templates/DashboardTemplate/index.js
+++ src/components/templates/DashboardTemplate/index.js
@@ -13,7 +13,7 @@ const Wrapper = styled.div`
   overflow: hidden;
   height: 1920px;
   background: linear-gradient(to right, #00928f 0%, #50b848 100%);
-  // cursor: url(data:image/gif;base64,R0lGODlhAQABAIAAAAAAAP///yH5BAEAAAAALAAAAAABAAEAAAIBRAA7), auto;
+  cursor: url(data:image/gif;base64,R0lGODlhAQABAIAAAAAAAP///yH5BAEAAAAALAAAAAABAAEAAAIBRAA7), auto;
 `
 
 const Content = styled.article`
